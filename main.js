902b16c0188ecef1b5d6c32167b83b3463087097
Steven Hawley <shawley@fulfillment.com>
2017-03-07 16:19:26 -0500

    Add environment. Only inject code when at the correct URL.

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const { BrowserWindow, app } = require('electron');
+const {BrowserWindow, app} = require('electron');
 const SerialPort = require('SerialPort');
 const path = require('path');
 const url = require('url');
 const regex = /(ST|US),GS,\s+([0-9.]+)(lb|kb)/g;
+const currentEnvironment = process.env.NODE_ENV;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-let webContents;
+let mainWindow, units, status, port;
+
 
 let windowOptions = {
   center: true,
@@ -16,47 +17,70 @@ let windowOptions = {
 };
 
 /** Serial Port Stuff **/
-let port = new SerialPort('/dev/tty.usbserial', {
-  parser: SerialPort.parsers.readline('\n'),
-  baudrate: 19200
-});
+function initSerialPort(path) {
+  port = new SerialPort(path, {
+    parser: SerialPort.parsers.readline('\n'),
+    baudrate: 19200
+  });
+}
 
 function readLine(line) {
   // console.log(line);
   let parsedLine = '0.000';
+  let stableString;
   while ((m = regex.exec(line)) !== null) {
     // This is necessary to avoid infinite loops with zero-width matches
     if (m.index === regex.lastIndex) {
       regex.lastIndex++;
     }
-
     // The result can be accessed through the `m`-variable.
     // m.forEach((match, groupIndex) => {
     //   console.log(`Found match, group ${groupIndex}: ${match}`);
     // });
+    units = m[3];
     parsedLine = m[2];
+    stableString = m[1];
   }
+  if (stableString !== undefined) {
+    status = stableString;
+  }
+  console.log("Status ", status);
   return parsedLine;
 }
 
-/** BrowerWindow setup and such **/
+
+/** BrowserWindow setup and such **/
 function createWindow() {
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 2000, height: 1800 });
+  mainWindow = new BrowserWindow({width: 2000, height: 1800});
+
+  // When in development environment, open the Redux DevTools Extension and the Chrome DevTools.
+  // Need to have the Chrome Extension at the location below.
+  // If on MAC and have Redux DevTools installed, then it should be at this location.
+  if (currentEnvironment == 'DEV') {
+    BrowserWindow.addDevToolsExtension("../../Library/Application Support/Google/Chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.14.1_0/");
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  }
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:3000/weighStation');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // Initialize the serial port
+  initSerialPort('/dev/tty.usbserial');
 
   mainWindow.webContents.on('did-finish-load', function () {
+    // Only inject code when they are on the correct web page
     // Stream all data coming in from the serial port.
     port.on('data', function (data) {
-      if (data) {
+      console.log(mainWindow.webContents.getURL());
+      if (data && mainWindow.webContents.getURL().includes('weighStation')) {
         let currentWeight = readLine(data);
-        console.log(currentWeight);
-        let code = `document.getElementById("weight").innerHTML = "${currentWeight}";`;
+        console.log("Current Weight: ", currentWeight);
+        let code = `document.getElementById("weight").innerHTML = "${currentWeight}";
+                    document.getElementById("status").innerHTML = "${status}";
+                    document.getElementById("units").innerHTML = "${units}"`;
         mainWindow.webContents.executeJavaScript(code);
       }
     });
