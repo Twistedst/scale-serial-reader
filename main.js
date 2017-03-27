694476455346ceb36fa0badab0ff5400f8cad104
Steven Hawley <shawley@fulfillment.com>
2017-03-27 16:14:59 -0400

    Check if elements exist before attempt setting.

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ if (setupEvents.handleSquirrelEvent()) {
 }
 
 const {BrowserWindow, app} = require('electron');
-const SerialPort = require('SerialPort');
-const path = require('path');
-const url = require('url');
-const regex = /(ST|US),GS,\s+([0-9.]+)(lb|kb)/g;
-const currentEnvironment = process.env.NODE_ENV;
+const SerialPort           = require('SerialPort');
+const path                 = require('path');
+const url                  = require('url');
+const regex                = /(ST|US),GS,\s+([0-9.]+)(lb|kb)/g;
+const currentEnvironment   = process.env.NODE_ENV;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -19,14 +19,14 @@ let mainWindow, units, status, port;
 
 let windowOptions = {
   center: true,
-  width: 2000,
+  width : 2200,
   height: 1800,
 };
 
 /** Serial Port Stuff **/
 function initSerialPort(path) {
   port = new SerialPort(path, {
-    parser: SerialPort.parsers.readline('\n'),
+    parser  : SerialPort.parsers.readline('\n'),
     baudrate: 19200
   });
 }
@@ -44,8 +44,8 @@ function readLine(line) {
     // m.forEach((match, groupIndex) => {
     //   console.log(`Found match, group ${groupIndex}: ${match}`);
     // });
-    units = m[3];
-    parsedLine = m[2];
+    units        = m[3];
+    parsedLine   = m[2];
     stableString = m[1];
   }
   if (stableString !== undefined) {
@@ -60,40 +60,48 @@ function readLine(line) {
 function createWindow() {
 
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 2000, height: 1800});
+  mainWindow = new BrowserWindow({width: 2200, height: 1800});
 
   // When in development environment, open the Redux DevTools Extension and the Chrome DevTools.
   // Need to have the Chrome Extension at the location below.
   // If on MAC and have Redux DevTools installed, then it should be at this location.
   if (currentEnvironment == 'DEV') {
-    BrowserWindow.addDevToolsExtension("../../Library/Application Support/Google/Chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.14.1_0/");
+    BrowserWindow.addDevToolsExtension("../../Library/Application Support/Google/Chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.14.2_0/");
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
   }
 
   // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000/weighStation');
+  mainWindow.loadURL('http://localhost:3000/');
 
   // Initialize the serial port
   initSerialPort('/dev/tty.usbserial');
 
   mainWindow.webContents.on('did-finish-load', function () {
-    // Only inject code when they are on the correct web page
-    // Stream all data coming in from the serial port.
-    port.on('data', function (data) {
-      console.log(mainWindow.webContents.getURL());
-      if (data && mainWindow.webContents.getURL().includes('weighStation')) {
+
+      // Only inject code when they are on the correct web page
+      // Stream all data coming in from the serial port.
+      port.on('data', function (data) {
         let currentWeight = readLine(data);
         console.log("Current Weight: ", currentWeight);
-        let code = `document.getElementById("weight").innerHTML = "${currentWeight}";
+        // console.log(mainWindow.webContents.getURL());
+        let code = `if(document.getElementById("weight") !== null){
+                    document.getElementById("weight").value = "${currentWeight}";
                     document.getElementById("status").innerHTML = "${status}";
-                    document.getElementById("units").innerHTML = "${units}"`;
-        mainWindow.webContents.executeJavaScript(code);
-      }
-    });
-  });
+                    document.getElementById("units").innerHTML = "${units}";
+                    };`;
 
-  // Emitted when the window is closed.
+        if (mainWindow.webContents.getURL().includes('tools/weighStation')) {
+          console.log("Execute");
+          mainWindow.webContents.executeJavaScript(code);
+        }
+      });
+
+    }
+  )
+  ;
+
+// Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
